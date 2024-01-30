3eb049f359581326545407cf3b89f5fc639e4992
camilarcampos <122387526+camilarcampos-ops@users.noreply.github.com>
2024-01-29 18:48:56 -0800

    update icon

--- src/Search.js
+++ src/Search.js
@@ -15,7 +15,7 @@ export default function Search(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "images/rainy-day.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
